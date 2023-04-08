ffa16625942614837348ac0c50e09a0ad064c985
Varun Viswanath <varunvis2903@gmail.com>
2023-04-09 00:30:28 +0530

    changes

--- src/views/sample-page/index.js
+++ src/views/sample-page/index.js
@@ -1,19 +1,28 @@
 /* eslint-disable prettier/prettier */
 // material-ui
 import { useState, useEffect } from 'react';
-import { Typography, TextField, OutlinedInput, InputLabel, FormControl, FormHelperText, Grid, Button, } from '@mui/material';
+import {
+    Typography,
+    TextField,
+    OutlinedInput,
+    InputLabel,
+    FormControl,
+    FormHelperText,
+    Grid,
+    Button,
+    Card,
+    CardMedia,
+    CardContent,
+    CardHeader,
+    CardActionArea
+} from '@mui/material';
+
 import { blue } from '@mui/material/colors';
 import { styled, useTheme } from '@mui/material/styles';
 import TotalIncomeDarkCard from 'views/dashboard/Default/TotalIncomeDarkCard';
-import {
-    ref,
-    uploadBytes,
-    getDownloadURL,
-    listAll,
-    list,
-} from "firebase/storage";
-import { v4 as uuidv4, v4 } from "uuid";
-import { storage } from "../../firebase/config";
+import { ref, uploadBytes, getDownloadURL, listAll, list } from 'firebase/storage';
+import { v4 as uuidv4, v4 } from 'uuid';
+import { storage } from '../../firebase/config';
 import SeonService from 'services/SeonService';
 
 // project imports
@@ -24,9 +33,13 @@ import MainCard from 'ui-component/cards/MainCard';
 const SamplePage = () => {
     const [load, setLoad] = useState(false);
     const [imageUrls, setImageUrls] = useState([]);
-    const [json, setJson] = useState("")
+    const [json, setJson] = useState('');
     const [imageUpload, setImageUpload] = useState();
-    const [userData, setUserData] = useState()
+    const [finalData, setFinalData] = useState([]);
+    const [linkedin, setLinkedin] = useState();
+    const [whatsapp, setWhatsapp] = useState();
+    const [breach, setBreach] = useState();
+
     const uploadFile = () => {
         setLoad(true);
         if (imageUpload == null) return;
@@ -42,26 +55,52 @@ const SamplePage = () => {
         const name = e.target.name;
         const value = e.target.value;
         setJson({ ...json, [name]: value });
-    }
-    console.log(json)
+    };
     const call = async () => {
-        await SeonService.seonEmailData(json)
-            .then((res) => {
-                console.log(res.data.data)
-                setUserData(res.data.data)
-                // setLoad(false)
-            })
-    }
-    console.log(json);
-    const theme = useTheme()
+        await SeonService.seonEmailData(json).then((res) => {
+            console.log(res.data.data);
+            setFinalData([res.data.data.account_details]);
+            setBreach([res.data.data.breach_details]);
+            setLinkedin(res.data.data.account_details.linkedin);
+            // setLoad(false)
+        });
+    };
+    const call1 = async () => {
+        await SeonService.seonPhoneData(json).then((res) => {
+            console.log(res.data.data);
+            setFinalData([res.data.data.account_details]);
+            setWhatsapp(res.data.data.account_details.whatsapp);
+            // setLoad(false)
+        });
+    };
+
+    console.log(finalData);
+
+    const getWhatsapp = async () => {
+        let res = await fetch(
+            'https://api.apify.com/v2/acts/inutil_labs~wscrp-free/runs?token=apify_api_d7Q3arjXU5CfOpR99cpeOhzUn8boR04B9RGP',
+            {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ numbers: [json] }),
+                mode: 'cors'
+            }
+        );
+        let data = await res.json();
+        console.log([data]);
+    };
+
+    console.log(finalData);
+    console.log(whatsapp);
+    const theme = useTheme();
     return (
         <>
             <MainCard title="Samle Card">
                 <Typography variant="body2">
-                    Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa. Ut enif
-                    ad minim venice, quin nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube grue dolor in
-                    reprehended in voltage veil esse colum doolie eu fujian bulla parian. Exceptive sin ocean cuspidate non president, sunk in culpa
-                    qui officiate descent molls anim id est labours.
+                    Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa.
+                    Ut enif ad minim venice, quin nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube
+                    grue dolor in reprehended in voltage veil esse colum doolie eu fujian bulla parian. Exceptive sin ocean cuspidate non
+                    president, sunk in culpa qui officiate descent molls anim id est labours.
                 </Typography>
             </MainCard>
 
@@ -70,8 +109,8 @@ const SamplePage = () => {
                     <Grid item xs={11}>
                         <FormControl
                             fullWidth
-                        // error={Boolean(touched.email && error s.email)} 
-                        // sx={{ ...theme.typography.customInput }}
+                            // error={Boolean(touched.email && error s.email)}
+                            // sx={{ ...theme.typography.customInput }}
                         >
                             <OutlinedInput
                                 id="outlined-adornment-email-login"
@@ -91,20 +130,97 @@ const SamplePage = () => {
                         </FormControl>
                     </Grid>
                     <Grid item xs={1}>
-                        <Button variant='outlined' sx={{ paddingBottom: 1, paddingTop: 1 }} onClick={call} >Upload</Button>
+                        <Button variant="outlined" sx={{ paddingBottom: 1, paddingTop: 1 }} onClick={call}>
+                            Upload
+                        </Button>
+                    </Grid>
+                </Grid>
+            </MainCard>
+            <MainCard sx={{ marginTop: 2 }} title="Firebase Upload Component">
+                <Grid alignItems="center" container spacing={2}>
+                    <Grid item xs={11}>
+                        <FormControl
+                            fullWidth
+                            // error={Boolean(touched.email && error s.email)}
+                            // sx={{ ...theme.typography.customInput }}
+                        >
+                            <OutlinedInput
+                                id="outlined-adornment-email-login"
+                                type="text"
+                                // value={values.email}
+                                name="phone_no"
+                                // onBlur={handleBlur}
+                                onChange={(e) => setJson(e.target.value)}
+                                label="Number"
+                                inputProps={{}}
+                            />
+                            {/* {touched.email && errors.email && (
+                        <FormHelperText error id="standard-weight-helper-text-email-login">
+                            {errors.email}
+                        </FormHelperText>
+                    )} */}
+                        </FormControl>
+                    </Grid>
+                    <Grid item xs={1}>
+                        <Button variant="outlined" sx={{ paddingBottom: 1, paddingTop: 1 }} onClick={getWhatsapp}>
+                            Upload
+                        </Button>
                     </Grid>
                 </Grid>
             </MainCard>
             <Grid container>
-
                 <Grid item xs={4}>
                     <TotalIncomeDarkCard />
                 </Grid>
-
             </Grid>
-
+            {linkedin ? (
+                <Card style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '1rem', maxWidth: 345 }}>
+                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '1rem' }}>
+                        <img
+                            src="https://cdn-icons-png.flaticon.com/512/174/174857.png"
+                            alt="LinkedIn"
+                            style={{ width: '2rem', height: '2rem', marginRight: '0.5rem' }}
+                        />
+                        <Typography variant="h5" component="h2" style={{ fontWeight: 'bold', fontSize: '1.5rem', letterSpacing: '0.1rem' }}>
+                            LinkedIn
+                        </Typography>
+                    </div>
+                    <CardMedia
+                        component="img"
+                        alt={linkedin.name}
+                        height="100"
+                        image={linkedin.photo}
+                        title={linkedin.name}
+                        style={{ borderRadius: '20%', width: '40%', margin: '0 auto', objectFit: 'cover' }}
+                    />
+                    <CardContent style={{ textAlign: 'center', marginTop: '1rem' }}>
+                        <Typography variant="h5" component="h2" style={{ fontWeight: 'bold', fontSize: '1.2rem' }}>
+                            {linkedin.name}
+                        </Typography>
+                        <Typography color="textSecondary" style={{ marginTop: '0.5rem' }}>
+                            {linkedin.title}
+                        </Typography>
+                        <Typography style={{ marginTop: '0.5rem' }}>{linkedin.location}</Typography>
+                        <Typography variant="h6" component="h3" style={{ marginTop: '1rem', fontWeight: 'bold' }}>
+                            {linkedin.registered ? 'Registered' : 'Not Registered'}
+                        </Typography>
+                    </CardContent>
+                </Card>
+            ) : null}
+            {whatsapp ? (
+                <Card sx={{ maxWidth: 345 }}>
+                    <CardActionArea>
+                        <CardMedia component="img" height="140" image={whatsapp.photo} alt="green iguana" />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                {whatsapp.about}
+                            </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                </Card>
+            ) : null}
         </>
-    )
+    );
 };
 
 export default SamplePage;
