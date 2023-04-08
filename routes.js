f51b1cc1d8fff8bc7ad1d74cdc435210b2c4ed43
lokita282 <lokitavarma282@gmail.com>
2023-04-08 17:39:50 +0530

    deployment for ip and email

--- routes.js
+++ routes.js
@@ -3,7 +3,40 @@ var router = express.Router()
 import request from 'request'
 
 
-router.get(`/:num`, function (req, res, next) {
+//email api
+router.get(`/email/:email`, function (req, res, next) {
+  var options = {
+    method: 'GET',
+    url: `https://api.seon.io/SeonRestService/email-api/v2.2/${req.params.email}`,
+    headers: {
+      'X-API-KEY': '6e7236ee-06a3-4046-aba8-35943eba2f17',
+      'Content-Type': 'application/json',
+    },
+  }
+  request(options, function (error, response) {
+    if (error) throw new Error(error)
+    console.log(response.body)
+  }).pipe(res)
+})
+
+//ip api 
+router.get(`/ip/:ip`, function (req, res, next) {
+  var options = {
+    method: 'GET',
+    url: `https://api.seon.io/SeonRestService/ip-api/v1.1/${req.params.ip}`,
+    headers: {
+      'X-API-KEY': '6e7236ee-06a3-4046-aba8-35943eba2f17',
+      'Content-Type': 'application/json',
+    },
+  }
+  request(options, function (error, response) {
+    if (error) throw new Error(error)
+    console.log(response.body)
+  }).pipe(res)
+})
+
+//phone number api
+router.get(`/phone/:num`, function (req, res, next) {
   var options = {
     method: 'GET',
     url: `https://api.seon.io/SeonRestService/phone-api/v1.4/${req.params.num}`,
@@ -18,7 +51,6 @@ router.get(`/:num`, function (req, res, next) {
   }).pipe(res)
 })
 
-// module.exports = router
 export default router
 
 
